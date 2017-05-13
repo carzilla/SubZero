609e6e5c99da102b74d49a99571b81fbfe0ced36
Carlos van der Putten <carlos@carzilla.nl>
2017-05-13 18:43:31 +0200

    Added documentation for the script

--- subzero.py
+++ subzero.py
@@ -1,12 +1,29 @@
-import sys, re
+"""
+subzero.py - script that fixes off sync srt files. Asks for the actual timestamp of the
+last item in the video file to use that timestamp to correct SRT subtitles.
 
-def readsrt(filename, lasttoxic = False):
+For help, suggestions, etc, feel free to contact me at carlos at carzilla.nl
+
+
+usage:
+python3 subzero.py <input file> <optional ignore last item>
+"""
+import sys
+import re
+
+def readsrt(filename, lasttoxic=False):
+    """
+    reads out srt file and if succesfully read, pushes it to fix method
+    :param filename filename to be read:
+    :param lasttoxic optional parameter if the last sub entry is toxic:
+    :return:
+    """
     last = -1
     if lasttoxic:
         last = -2
     print("Opening {}".format(filename))
-    f = open(filename, 'r')
-    srt = f.read()
+    file = open(filename, 'r')
+    srt = file.read()
     srt_list = srt.split('\n\n')
     while srt_list[last] == "":
         last = last -1
@@ -18,32 +35,49 @@ def readsrt(filename, lasttoxic = False):
     last_line = srt_list[last].split('\n')
     timestamps = re.findall(regex, last_line[1])
     timestamps_list = timestamps[0].split(":")
-    last_line_s = (int(timestamps_list[0]) * 3600) + (int(timestamps_list[1]) * 60) + int(timestamps_list[2])
+    last_line_s = (int(timestamps_list[0]) * 3600) + \
+                  (int(timestamps_list[1]) * 60) + int(timestamps_list[2])
     found_s = (int(found_list[0]) * 3600) + (int(found_list[1]) * 60) + int(found_list[2])
     offset = found_s - last_line_s
     offset_per_s = last_line_s / offset
     fixsrt(srt_list, offset_per_s)
 
 def timestamp_to_s(timestamp):
+    """
+    Converts timestamps to second formats
+    :param timestamp:
+    :return:
+    """
     tmp = timestamp.split(":")
     return (int(tmp[0]) * 3600) + (int(tmp[1]) * 60) + (int(tmp[2]))
 
-def s_to_timestamp(s):
-    hours = s / 3600
+def s_to_timestamp(time_s):
+    """
+    Converts seconds to timestamp format
+    :param time_s time in seconds:
+    :return:
+    """
+    hours = time_s / 3600
     if hours < 1:
         hours = 0
-    minutes = int(s / 60)
+    minutes = int(time_s / 60)
     if minutes > 60:
         minutes = 60
 
     tmp = (hours * 3600) + (minutes * 60)
-    seconds = int(s - tmp)
+    seconds = int(time_s - tmp)
 
     return "{:02d}:{:02d}:{:02d},000".format(hours, minutes, seconds)
 
 
 def fixsrt(srt, offset_per_s):
-    f = open('fixed.srt', 'w')
+    """
+    Generates new SRT with the old SRT as list
+    :param srt old srt list, gotten from readsrt:
+    :param offset_per_s offset calculated in readsrt in seconds:
+    :return:
+    """
+    newfile = open('fixed.srt', 'w')
     regex = r"(\d.:\d.:\d.)"
     for item in srt:
         tmp = item.split("\n")
@@ -57,8 +91,8 @@ def fixsrt(srt, offset_per_s):
         new = new + new_start + " --> " + new_end + "\n"
         new = new + "\n".join(tmp[2:])
         new = new + "\n\n"
-        f.write(new)
-    f.close()
+        newfile.write(new)
+    newfile.close()
 
 
 if __name__ == "__main__":
